62470dfd4f1faa7e975b025b5e5580e4b5b212b4
majidkawouk <57314886+majidkawouk@users.noreply.github.com>
2025-04-15 18:38:22 +0300

    changes

---
+++ app/createdeck/page.js
@@ -0,0 +1,13 @@
+import Creatdeck from "@/components/Createdeck";
+import Head from "@/components/Head";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head></Head>
+        <Creatdeck></Creatdeck>
+    </div>
+  );
+}
---
+++ components/Createdeck.jsx
@@ -0,0 +1,16 @@
+"use client"
+import { useEffect, useState } from "react";
+import { decks as defaultDecks } from "./decks"; // your original array
+
+export default function DecksPage() {
+    const [deckname,setdeckname] = useState("");
+    const [color,setcolor] = useState("");
+    const [emoji,setemoji] = useState("");
+    const [cardnumber,setcardnumber] = useState("");
+    const [question,setquestion] = useState("");
+    const [answer,setanswer] = useState("");
+
+        return (<div>
+
+    </div>)
+}
