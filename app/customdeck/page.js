c68546cc8b75d9bc7551a37fe28859a686d86a45
majidkawouk <57314886+majidkawouk@users.noreply.github.com>
2025-04-21 22:39:09 +0300

    Update page.js

--- app/customdeck/page.js
+++ app/customdeck/page.js
@@ -1,6 +1,6 @@
 import Cdeck from "@/components/Cdeck";
 import Head from "@/components/Head";
-import FlipCard from "@/components/FlipCard";
+
 import Image from "next/image";
 
 export default function Home() {
